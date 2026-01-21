7130f035a5de814734e769973dd12c8a48305105
Suraj Kumar <suraj71308kumar@gmail.com>
2026-01-21 15:00:15 +0530

    added blur bg-color for view all buttons, featured section and minor improvment in the navbar.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
         <Link
           to={"/"}
           onClick={() => {
-            scrollTo(0, 0), setIsOpen(false);
+            scrollTo(0, 0); setIsOpen(false);
           }}
         >
           Home
@@ -39,7 +39,7 @@ const Navbar = () => {
         <Link
           to={"/movies"}
           onClick={() => {
-            scrollTo(0, 0), setIsOpen(false);
+            scrollTo(0, 0); setIsOpen(false);
           }}
         >
           Movies
@@ -47,7 +47,7 @@ const Navbar = () => {
         <Link
           to={"/"}
           onClick={() => {
-            scrollTo(0, 0), setIsOpen(false);
+            scrollTo(0, 0); setIsOpen(false);
           }}
         >
           Theaters
@@ -55,7 +55,7 @@ const Navbar = () => {
         <Link
           to={"/"}
           onClick={() => {
-            scrollTo(0, 0), setIsOpen(false);
+            scrollTo(0, 0); setIsOpen(false);
           }}
         >
           Releases
@@ -63,7 +63,7 @@ const Navbar = () => {
         <Link
           to={"/favorite"}
           onClick={() => {
-            scrollTo(0, 0), setIsOpen(false);
+            scrollTo(0, 0); setIsOpen(false);
           }}
         >
           Favorites
